9f5c0225b6be712c7f14c91ee2dc33d36e175f1d
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2023-10-08 11:16:25 -0300

    feat: add select and deselect all button

--- src/TabList.tsx
+++ src/TabList.tsx
@@ -5,13 +5,14 @@ import {
     AccordionItem,
     AccordionPanel,
     Box,
+    Button,
     Checkbox,
     CheckboxGroup,
     Icon,
     Image,
     Text,
 } from "@chakra-ui/react"
-import React from "react"
+import React, { useCallback } from "react"
 import { BsIncognito } from "react-icons/bs"
 import { colors } from "./style/colors"
 import { Tab } from "./types/Tab"
@@ -23,66 +24,93 @@ type Props = {
 }
 
 export const TabList = ({ tabs, selectedIds, setSelectedIds }: Props) => {
+    const isAllSelected = tabs.length === selectedIds.length
+
+    const handleSelectAll = useCallback(() => {
+        const allIds = tabs.filter((tab) => tab.id).map((tab) => String(tab.id)) as string[]
+
+        setSelectedIds(allIds)
+    }, [setSelectedIds, tabs])
+
+    const handleUnselectAll = useCallback(() => {
+        setSelectedIds([])
+    }, [setSelectedIds])
+
     return (
-        <Accordion allowToggle defaultIndex={[0]}>
-            <AccordionItem>
-                <AccordionButton
-                    paddingX={0}
-                    className="flex"
-                    alignItems="center"
-                    justifyContent="space-between"
-                >
-                    <Box className="flex">
-                        <AccordionIcon />
+        <>
+            <Button
+                size="xs"
+                _hover={{ bgColor: colors.green700, color: colors.white }}
+                bgColor={colors.green700}
+                color={colors.white}
+                onClick={isAllSelected ? handleUnselectAll : handleSelectAll}
+            >
+                {isAllSelected ? "deselect all" : "select all"}
+            </Button>
+            <Accordion allowToggle defaultIndex={[0]}>
+                <AccordionItem>
+                    <AccordionButton
+                        paddingX={0}
+                        className="flex"
+                        alignItems="center"
+                        justifyContent="space-between"
+                    >
+                        <Box className="flex">
+                            <AccordionIcon />
+                            <Text fontSize={14} color={colors.green700} fontWeight={700}>
+                                Tabs
+                            </Text>
+                        </Box>
                         <Text fontSize={14} color={colors.green700} fontWeight={700}>
-                            Tabs
+                            {`${selectedIds.length} selected`}
                         </Text>
-                    </Box>
-                    <Text fontSize={14} color={colors.green700} fontWeight={700}>
-                        {`${selectedIds.length} selected`}
-                    </Text>
-                </AccordionButton>
-                <AccordionPanel maxH="250px" overflowY="auto">
-                    <CheckboxGroup onChange={setSelectedIds} value={selectedIds}>
-                        {tabs.map((tab) => (
-                            <Checkbox
-                                borderColor={colors.green700}
-                                size="sm"
-                                w="100%"
-                                key={tab.id}
-                                value={String(tab.id)}
-                                _checked={{
-                                    "& .chakra-checkbox__control": {
-                                        borderColor: colors.green700,
-                                        background: colors.green700,
-                                    },
-                                }}
-                            >
-                                <Box className="flex" alignItems="center">
-                                    <Image
-                                        boxSize="15px"
-                                        marginRight="6px"
-                                        src={tab.favIconUrl}
-                                        alt="tab icon"
-                                        fallbackSrc="https://via.placeholder.com/15"
-                                    />
-                                    <Text
-                                        color={colors.green700}
-                                        fontWeight={700}
-                                        fontSize={12}
-                                        noOfLines={2}
-                                    >
-                                        {tab.title}
-                                    </Text>
-                                    {tab.incognito && (
-                                        <Icon as={BsIncognito} marginLeft="8px" fontSize="12px" />
-                                    )}
-                                </Box>
-                            </Checkbox>
-                        ))}
-                    </CheckboxGroup>
-                </AccordionPanel>
-            </AccordionItem>
-        </Accordion>
+                    </AccordionButton>
+                    <AccordionPanel maxH="250px" overflowY="auto">
+                        <CheckboxGroup onChange={setSelectedIds} value={selectedIds}>
+                            {tabs.map((tab) => (
+                                <Checkbox
+                                    borderColor={colors.green700}
+                                    size="sm"
+                                    w="100%"
+                                    key={tab.id}
+                                    value={String(tab.id)}
+                                    _checked={{
+                                        "& .chakra-checkbox__control": {
+                                            borderColor: colors.green700,
+                                            background: colors.green700,
+                                        },
+                                    }}
+                                >
+                                    <Box className="flex" alignItems="center">
+                                        <Image
+                                            boxSize="15px"
+                                            marginRight="6px"
+                                            src={tab.favIconUrl}
+                                            alt="tab icon"
+                                            fallbackSrc="https://via.placeholder.com/15"
+                                        />
+                                        <Text
+                                            color={colors.green700}
+                                            fontWeight={700}
+                                            fontSize={12}
+                                            noOfLines={2}
+                                        >
+                                            {tab.title}
+                                        </Text>
+                                        {tab.incognito && (
+                                            <Icon
+                                                as={BsIncognito}
+                                                marginLeft="8px"
+                                                fontSize="12px"
+                                            />
+                                        )}
+                                    </Box>
+                                </Checkbox>
+                            ))}
+                        </CheckboxGroup>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+        </>
     )
 }
